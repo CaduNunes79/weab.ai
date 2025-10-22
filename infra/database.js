177e8c56d85b7307e02d2bbabba17f642107a09a
Cadu Nunes <nunes.cadu1979@gmail.com>
2025-10-21 21:40:33 -0300

    Add .env environment variables file & jest.config.js file on project

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Client, Query } from "pg";
+import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
@@ -7,21 +7,74 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
-  try {
-    await client.connect();
-    const result = await client.query(queryObject);
-    return result;
-  } catch {
-    console.error(error);
-    throw error;
-  } finally {
-    await client.end();
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+
+  return result;
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
   }
 }
 
 export default {
   query: query,
 };
+
+// import { Client } from "pg";
+
+// async function query(queryObject) {
+//   let client;
+//   try {
+//     client = await getNewClient();
+//     const result = await client.query(queryObject);
+//     return result;
+//   } catch (error) {
+//     const serviceErrorObject = new ServiceError({
+//       message: "Erro na conexão com Banco ou na Query.",
+//       cause: error,
+//     });
+//     throw serviceErrorObject;
+//   } finally {
+//     await client.end();
+//   }
+// }
+
+// async function getNewClient() {
+//   const client = new Client({
+//     host: process.env.POSTGRES_HOST,
+//     port: process.env.POSTGRES_PORT,
+//     user: process.env.POSTGRES_USER,
+//     database: process.env.POSTGRES_DB,
+//     password: process.env.POSTGRES_PASSWORD,
+//     ssl: getSSLValues(),
+//   });
+
+//   await client.connect();
+//   return client;
+// }
+
+// const database = {
+//   query,
+//   getNewClient,
+// };
+
+// export default database;
+
+// function getSSLValues() {
+//   if (process.env.POSTGRES_CA) {
+//     return {
+//       ca: process.env.POSTGRES_CA,
+//     };
+//   }
+
+//   return process.env.NODE_ENV === "production" ? true : false;
+//}
