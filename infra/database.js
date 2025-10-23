f235079bc3888aec540b4081221e4d9b85531b59
Cadu Nunes <nunes.cadu1979@gmail.com>
2025-10-23 14:52:19 -0300

    Add tests support on database.js

--- infra/database.js
+++ infra/database.js
@@ -10,11 +10,15 @@ async function query(queryObject) {
     ssl: getSSLValues(),
   });
 
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-
-  return result;
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 function getSSLValues() {
@@ -23,58 +27,10 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
+
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
 export default {
   query: query,
 };
-
-// import { Client } from "pg";
-
-// async function query(queryObject) {
-//   let client;
-//   try {
-//     client = await getNewClient();
-//     const result = await client.query(queryObject);
-//     return result;
-//   } catch (error) {
-//     const serviceErrorObject = new ServiceError({
-//       message: "Erro na conexão com Banco ou na Query.",
-//       cause: error,
-//     });
-//     throw serviceErrorObject;
-//   } finally {
-//     await client.end();
-//   }
-// }
-
-// async function getNewClient() {
-//   const client = new Client({
-//     host: process.env.POSTGRES_HOST,
-//     port: process.env.POSTGRES_PORT,
-//     user: process.env.POSTGRES_USER,
-//     database: process.env.POSTGRES_DB,
-//     password: process.env.POSTGRES_PASSWORD,
-//     ssl: getSSLValues(),
-//   });
-
-//   await client.connect();
-//   return client;
-// }
-
-// const database = {
-//   query,
-//   getNewClient,
-// };
-
-// export default database;
-
-// function getSSLValues() {
-//   if (process.env.POSTGRES_CA) {
-//     return {
-//       ca: process.env.POSTGRES_CA,
-//     };
-//   }
-
-//   return process.env.NODE_ENV === "production" ? true : false;
-//}
--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,14 @@ const dotenv = require("dotenv");
 dotenv.config({
   path: ".env.development",
 });
+
 const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   dir: ".",
+  setTimeout: 50000,
 });
+
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
 });
